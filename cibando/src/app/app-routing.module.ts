61db35f00085f9c89bbeb9d78724c2522bf65109
Danilo-jdk <danilodefilio.jdk@gmail.com>
2022-11-11 13:35:15 +0100

    aggiunti i path children nel routing

--- cibando/src/app/app-routing.module.ts
+++ cibando/src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
 import { DetailComponent } from './components/recipes/detail/detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
-  { path: 'ricette', component: RecipesComponent},
-  { path: 'dettaglio/:_id', component: DetailComponent},
+  { path: 'ricette', component: RecipesComponent, children: [
+    { path: 'dettaglio/:title/:_id', component: DetailComponent},
+    { path: '', pathMatch: 'full', component: RecipesListComponent}
+  ]},
+  // { path: 'ricette', component: RecipesComponent},
+  // { path: 'dettaglio/:_id', component: DetailComponent},
   { path: '**', redirectTo: 'home'}
 ];
 
--- cibando/src/app/components/recipes/recipes.component.ts
+++ cibando/src/app/components/recipes/recipes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from 'src/app/models/recipe.model';
-import { RecipeService } from 'src/app/services/recipe.service';
+
 
 @Component({
   selector: 'app-recipes',
@@ -8,22 +7,10 @@ import { RecipeService } from 'src/app/services/recipe.service';
   styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent implements OnInit {
-  ricette: Recipe[] = [];
 
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    console.log('sei entrato in ricette');
-
-    this.recipeService.getRecipes().subscribe({
-      next: (res) => {
-        this.ricette = res;
-      },
-      error: (e) => {
-        console.error(e)
-      }
-    })
-
 
   }
 
