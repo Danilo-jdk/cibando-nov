e3641c69a783aad8d745682b6e9fbe64636594d9
Danilo-jdk <danilodefilio.jdk@gmail.com>
2022-11-26 10:38:36 +0100

    fatto il lazy loading

--- cibando/src/app/app.module.ts
+++ cibando/src/app/app.module.ts
@@ -13,6 +13,9 @@ import {PaginatorModule} from 'primeng/paginator';
 import { HttpClientModule } from '@angular/common/http';
 import {ToastModule} from 'primeng/toast';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { MessageService } from 'primeng/api';
+
+import { RecipesModule } from './components/recipes/recipes.module';
 
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
@@ -20,18 +23,12 @@ import { RecipesComponent } from './components/recipes/recipes.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { RecipeCardComponent } from './shared/recipe-card/recipe-card.component';
-import { DetailComponent } from './components/recipes/detail/detail.component';
-import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
 import { RegistrationComponent } from './components/user/registration/registration.component';
 import { ChangeColorDirective } from './directives/change-color.directive';
 import { EsempioComponent } from './components/esempio/esempio.component';
 import { LoginComponent } from './components/user/login/login.component';
-import { MessageService } from 'primeng/api';
 import { ProfileComponent } from './components/user/profile/profile.component';
 import { VideoComponent } from './components/video/video.component';
-import { NuovaRicettaComponent } from './components/recipes/nuova-ricetta/nuova-ricetta.component';
-import { ResultComponent } from './components/recipes/result/result.component';
 
 
 
@@ -43,17 +40,12 @@ import { ResultComponent } from './components/recipes/result/result.component';
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    RecipeCardComponent,
-    DetailComponent,
-    RecipesListComponent,
     RegistrationComponent,
     ChangeColorDirective,
     EsempioComponent,
     LoginComponent,
     ProfileComponent,
     VideoComponent,
-    NuovaRicettaComponent,
-    ResultComponent
   ],
   imports: [
     BrowserModule,
@@ -70,8 +62,10 @@ import { ResultComponent } from './components/recipes/result/result.component';
     HttpClientModule,
     PaginatorModule,
     ToastModule,
-    CKEditorModule
+    CKEditorModule,
+    RecipesModule
   ],
+  // schemas: [ CUSTOM_ELEMENTS_SCHEMA ], // Must add this one.
   providers: [ MessageService],
   bootstrap: [AppComponent]
 })
